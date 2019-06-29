bbadabb5f33bc10a2d37c765c12513c4d6abc26d
Phernandoe <fbarreto@ithaca.edu>
2019-06-29 16:01:35 -0400

    Added keys for lists

--- components/ProjectsContent.js
+++ components/ProjectsContent.js
@@ -3,15 +3,15 @@ import Link from 'next/link';
 
 const ProjectsContent = ({projects}) => (
   <React.Fragment>
-    {projects.map( project => (
-      <ul>
-        <li>
+    {projects.map( (project, index) => (
+      <ul key={index}>
+        <li key={project.title}>
           <Link href={project.link}> 
             <a target='_blank'>{project.title}</a>
           </Link>
         </li>
-        <li>{project.tools}</li>
-        <li>{project.desc}</li>
+        <li key={project.tools}>{project.tools}</li>
+        <li key={project.desc}>{project.desc}</li>
       </ul>
     ))}
   </React.Fragment>
