436e3e0d69596b221eaf9930abdb8fbe6882fe85
Phernandoe <fbarreto@ithaca.edu>
2019-07-03 16:26:43 -0400

    Modifed move-redirect

--- move-redirect.js
+++ move-redirect.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 
 fs.copyFile('_redirects', './out/_redirects', err => {
-if (err) throw err;
+  if (err) {
+    console.log('There was an error moving the _redirect file', err);
+    throw err;
+  } 
+  
   console.log('_redirects was copied to ./out/_redirects');
 });
