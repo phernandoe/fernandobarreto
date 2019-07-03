375057ad75113417c68b418feb5390f4d0ac9908
Phernandoe <fbarreto@ithaca.edu>
2019-07-03 18:50:12 -0400

    ...

--- next.config.js
+++ next.config.js
@@ -1,2 +1,14 @@
 const withCSS = require('@zeit/next-css');
 module.exports = withCSS();
+
+module.exports = {
+  exportPathMap: async function(defaultPathMap) {
+    return {
+      '/': { page: '/' },
+      '/contact': { page: '/contact' },
+      '/skills': { page: '/skills' },
+      '/projects': { page: '/projects' }
+    };
+  }
+}
+
