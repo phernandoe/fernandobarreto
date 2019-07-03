a74c6bc1b54c3af7c06900124531b9fa5ed6e32f
Phernandoe <fbarreto@ithaca.edu>
2019-07-03 18:59:11 -0400

    ...

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ module.exports = withCSS(),
   exportPathMap: async function(defaultPathMap) {
     return {
       '/': { page: '/index' },
-      '/contact': { page: '/Contact' },
-      '/skills': { page: '/Skills.js' },
+      '/contact/': { page: '/Contact' },
+      '/skills/': { page: '/Skills.js' },
       '/projects': { page: '/Projects' }
     };
   }
